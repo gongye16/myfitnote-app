36036c9c4ae31eef407cbd0dcfdc42582df769af
EC2 Default User <ec2-user@ip-172-31-92-3.ec2.internal>
2025-05-14 03:08:23 +0000

    Add dotenv and update app configuration

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const util = require('util');
+const logFile = fs.createWriteStream('./logs/app.log', { flags: 'a' });
+const logStdout = process.stdout;
+
+console.log = function () {
+  logFile.write(util.format.apply(null, arguments) + '\n');
+  logStdout.write(util.format.apply(null, arguments) + '\n');
+};
+
+
 const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
 app.use(express.json());
 
+console.log("MyFitNote app is running and logging successfully!");
+
+
 // MySQL接続設定
 const db = mysql.createConnection({
-  host: 'myfitnote-db.cxuya6m2ioxl.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: '123qwecc',
-  database: 'myfitnote'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
-  if (err) throw err;
+  if (err) {
+  console.error('MySQL接続失敗:', err.message);
+  return;
+ }
   console.log('MySQL connected!');
 });
 
 // ルート表示
 app.get('/', (req, res) => {
-  res.send('<h1>筋トレToDo APIが起動中??</h1>');
+
+  res.send('<h1>筋トレToDo APIが起動中!!</h1>');
 });
 
 // ToDo取得
@@ -40,7 +60,9 @@ app.post('/todos', (req, res) => {
 });
 
 // サーバ起動
-app.listen(80, () => {
-  console.log('Server is running on port 80');
+const PORT = process.env.PORT || 3000;
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
 
